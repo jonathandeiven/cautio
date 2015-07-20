782c67ef26043c8c144c94f00b6aeefd8b4788b6
Karthiga <karthiga.ve@gmail.com>
2015-07-19 21:05:29 -0400

    4 graphs. 1 page
    nuff said

--- webapp/js/graph-main.js
+++ webapp/js/graph-main.js
@@ -8,15 +8,114 @@ document.ready = function () {
                 "#92D050"                
                 ]);
 
+// LUX METER DATA
 		// dataPoints
-		var dataPoints1 = [];
-		var dataPoints2 = [];
-		var dataPoints3 = [];
-		var line1 = "x";
-		var line2 = "y";
-		var line3 = "z";
+		var LUMdataPoints1 = [];
+		var LUMline1 = "x";
 
-		var chart = new CanvasJS.Chart("chart-accel",{
+		var LUMchart = new CanvasJS.Chart("chart-lum",{
+			colorSet: "Shades",
+			backgroundColor: "#f1f1f1",
+			zoomEnabled: true,
+			toolTip: {
+				shared: true
+				
+			},
+			legend: {
+				verticalAlign: "top",
+				horizontalAlign: "center",
+                                fontSize: 14,
+				fontWeight: "bold",
+				fontFamily: "calibri",
+				fontColor: "dimGrey"
+			},
+			axisX: {
+				//title: "Time"
+			},
+			axisY:{
+				//title: "Acceleration",
+				includeZero: false,
+				gridDashType: "dot" 
+			}, 
+			data: [{ 
+				// dataSeries1
+				type: "spline",
+				xValueType: "number",
+				name: LUMline1 ,
+				dataPoints: LUMdataPoints1
+			}],
+          legend:{
+			dockInsidePlotArea: true,
+			horizontalAlign: "right",
+            cursor:"pointer",
+            itemclick : function(e) {
+              if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                e.dataSeries.visible = false;
+              }
+              else {
+                e.dataSeries.visible = true;
+              }
+              LUMchart.render();
+            }
+          }
+		});
+
+		var LUMupdateInterval = 1000;
+		// initial value
+		var LUMyValue1 = 0; 
+
+		var LUMtime = 0;
+
+		var LUMupdateChart = function (LUMcount) {
+			LUMcount = LUMcount || 1;
+
+			// count is number of times loop runs to generate random dataPoints. 
+
+			for (var i = 0; i < LUMcount; i++) {
+				
+				// add interval duration to time				
+				LUMtime++;
+
+
+				// generating random values
+				var LUMdeltaY1 = .5 + Math.random() *(-.5-.5);
+
+				// adding random value and rounding it to two digits. 
+				LUMyValue1 = Math.round((LUMyValue1 + LUMdeltaY1)*100)/100;
+				
+				// pushing the new values
+				LUMdataPoints1.push({
+					x: LUMtime,
+					y: LUMyValue1
+				});
+
+				// Delete old data to prevent build up
+				if (LUMdataPoints1.length > 30) {
+					LUMdataPoints1.shift();
+				}
+
+			};
+
+			LUMchart.render();
+
+		};
+
+		// generates first set of dataPoints 
+		LUMupdateChart(30);	
+		 
+		// update chart after specified interval 
+		setInterval(function(){LUMupdateChart()}, LUMupdateInterval);
+
+// ACCELEROMETER DATA
+				// dataPoints
+		var ACCELdataPoints1 = [];
+		var ACCELdataPoints2 = [];
+		var ACCELdataPoints3 = [];
+		var ACCELline1 = "x";
+		var ACCELline2 = "y";
+		var ACCELline3 = "z";
+
+		var ACCELchart = new CanvasJS.Chart("chart-accel",{
 			colorSet: "Shades",
 			backgroundColor: "#f1f1f1",
 			zoomEnabled: true,
@@ -45,24 +144,24 @@ document.ready = function () {
 				type: "line",
 				xValueType: "number",
 				showInLegend: true,
-				name: line1 ,
-				dataPoints: dataPoints1
+				name: ACCELline1 ,
+				dataPoints: ACCELdataPoints1
 			},
 			{				
 				// dataSeries2
 				type: "line",
 				xValueType: "number",
 				showInLegend: true,
-				name: line2 ,
-				dataPoints: dataPoints2
+				name: ACCELline2 ,
+				dataPoints: ACCELdataPoints2
 			},
 			{				
 				// dataSeries3
 				type: "line",
 				xValueType: "number",
 				showInLegend: true,
-				name: line3 ,
-				dataPoints: dataPoints3
+				name: ACCELline3 ,
+				dataPoints: ACCELdataPoints3
 			}],
           legend:{
 			dockInsidePlotArea: true,
@@ -75,87 +174,311 @@ document.ready = function () {
               else {
                 e.dataSeries.visible = true;
               }
-              chart.render();
+              ACCELchart.render();
             }
           }
 		});
 
-		var updateInterval = 1000;
+		var ACCELupdateInterval = 1000;
 		// initial value
-		var yValue1 = 0; 
-		var yValue2 = 0;
-		var yValue3 = 0;
+		var ACCELyValue1 = 0; 
+		var ACCELyValue2 = 0; 
+		var ACCELyValue3 = 0; 
 
-		var time = 0;
-		/*time.setHours(9);
-		time.setMinutes(30);
-		time.setSeconds(00);
-		time.setMilliseconds(00);*/
-		// starting at 9.30 am
+		var ACCELtime = 0;
 
-		var updateChart = function (count) {
-			count = count || 1;
+		var ACCELupdateChart = function (ACCELcount) {
+			ACCELcount = ACCELcount || 1;
 
 			// count is number of times loop runs to generate random dataPoints. 
 
-			for (var i = 0; i < count; i++) {
+			for (var i = 0; i < ACCELcount; i++) {
 				
 				// add interval duration to time				
-				time++;
+				ACCELtime++;
 
 
 				// generating random values
-				var deltaY1 = .5 + Math.random() *(-.5-.5);
-				var deltaY2 = .5 + Math.random() *(-.5-.5);
-				var deltaY3 = .5 + Math.random() *(-.5-.5);
+				var ACCELdeltaY1 = .5 + Math.random() *(-.5-.5);
+				var ACCELdeltaY2 = .5 + Math.random() *(-.5-.5);
+				var ACCELdeltaY3 = .5 + Math.random() *(-.5-.5);
 
 				// adding random value and rounding it to two digits. 
-				yValue1 = Math.round((yValue1 + deltaY1)*100)/100;
-				yValue2 = Math.round((yValue2 + deltaY2)*100)/100;
-				yValue3 = Math.round((yValue3 + deltaY3)*100)/100;
+				ACCELyValue1 = Math.round((ACCELyValue1 + ACCELdeltaY1)*100)/100;
+				ACCELyValue2 = Math.round((ACCELyValue2 + ACCELdeltaY2)*100)/100;
+				ACCELyValue3 = Math.round((ACCELyValue3 + ACCELdeltaY3)*100)/100;
 				
 				// pushing the new values
-				dataPoints1.push({
-					x: time,
-					y: yValue1
+				ACCELdataPoints1.push({
+					x: ACCELtime,
+					y: ACCELyValue1
 				});
-				dataPoints2.push({
-					x: time,
-					y: yValue2
+				ACCELdataPoints2.push({
+					x: ACCELtime,
+					y: ACCELyValue2
 				});
-				dataPoints3.push({
-					x: time,
-					y: yValue3
+				ACCELdataPoints3.push({
+					x: ACCELtime,
+					y: ACCELyValue3
 				});
 
 				// Delete old data to prevent build up
-				if (dataPoints1.length > 30) {
-					dataPoints1.shift();
+				if (ACCELdataPoints1.length > 30) {
+					ACCELdataPoints1.shift();
 				}
-				if (dataPoints2.length > 30) {
-					dataPoints2.shift();
+				if (ACCELdataPoints2.length > 30) {
+					ACCELdataPoints2.shift();
 				}
-				if (dataPoints3.length > 30) {
-					dataPoints3.shift();
+				if (ACCELdataPoints3.length > 30) {
+					ACCELdataPoints3.shift();
 				}
 
 			};
 
-			
-			
-			
-			/*// updating legend text with  updated with y Value 
-			chart.options.data[0].legendText = line1 + "  $" + yValue1;
-			chart.options.data[1].legendText = line2 + "  $" + yValue2; 
-			chart.options.data[2].legendText = line3 + "  $" + yValue3; */
-
-			chart.render();
+			ACCELchart.render();
 
 		};
 
 		// generates first set of dataPoints 
-		updateChart(30);	
+		ACCELupdateChart(30);	
 		 
 		// update chart after specified interval 
-		setInterval(function(){updateChart()}, updateInterval);
-	}
+		setInterval(function(){ACCELupdateChart()}, ACCELupdateInterval);
+		
+// TEMPERATURE DATA
+		// dataPoints
+		var TEMPdataPoints1 = [];
+		var TEMPline1 = "x";
+
+		var TEMPchart = new CanvasJS.Chart("chart-temp",{
+			colorSet: "Shades",
+			backgroundColor: "#f1f1f1",
+			zoomEnabled: true,
+			toolTip: {
+				shared: true
+				
+			},
+			legend: {
+				verticalAlign: "top",
+				horizontalAlign: "center",
+                                fontSize: 14,
+				fontWeight: "bold",
+				fontFamily: "calibri",
+				fontColor: "dimGrey"
+			},
+			axisX: {
+				//title: "Time"
+			},
+			axisY:{
+				//title: "Acceleration",
+				includeZero: false,
+				gridDashType: "dot" 
+			}, 
+			data: [{ 
+				// dataSeries1
+				type: "spline",
+				xValueType: "number",
+				name: TEMPline1 ,
+				dataPoints: TEMPdataPoints1
+			}],
+          legend:{
+			dockInsidePlotArea: true,
+			horizontalAlign: "right",
+            cursor:"pointer",
+            itemclick : function(e) {
+              if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                e.dataSeries.visible = false;
+              }
+              else {
+                e.dataSeries.visible = true;
+              }
+              TEMPchart.render();
+            }
+          }
+		});
+
+		var TEMPupdateInterval = 1000;
+		// initial value
+		var TEMPyValue1 = 0; 
+
+		var TEMPtime = 0;
+
+		var TEMPupdateChart = function (TEMPcount) {
+			TEMPcount = TEMPcount || 1;
+
+			// count is number of times loop runs to generate random dataPoints. 
+
+			for (var i = 0; i < TEMPcount; i++) {
+				
+				// add interval duration to time				
+				TEMPtime++;
+
+
+				// generating random values
+				var TEMPdeltaY1 = .5 + Math.random() *(-.5-.5);
+
+				// adding random value and rounding it to two digits. 
+				TEMPyValue1 = Math.round((TEMPyValue1 + TEMPdeltaY1)*100)/100;
+				
+				// pushing the new values
+				TEMPdataPoints1.push({
+					x: TEMPtime,
+					y: TEMPyValue1
+				});
+
+				// Delete old data to prevent build up
+				if (TEMPdataPoints1.length > 30) {
+					TEMPdataPoints1.shift();
+				}
+
+			};
+
+			TEMPchart.render();
+
+		};
+
+		// generates first set of dataPoints 
+		TEMPupdateChart(30);	
+		 
+		// update chart after specified interval 
+		setInterval(function(){TEMPupdateChart()}, TEMPupdateInterval);
+
+// GYROSCOPE DATA
+				// dataPoints
+		var GYROdataPoints1 = [];
+		var GYROdataPoints2 = [];
+		var GYROdataPoints3 = [];
+		var GYROline1 = "x";
+		var GYROline2 = "y";
+		var GYROline3 = "z";
+
+		var GYROchart = new CanvasJS.Chart("chart-gyro",{
+			colorSet: "Shades",
+			backgroundColor: "#f1f1f1",
+			zoomEnabled: true,
+			toolTip: {
+				shared: true
+				
+			},
+			legend: {
+				verticalAlign: "top",
+				horizontalAlign: "center",
+                                fontSize: 14,
+				fontWeight: "bold",
+				fontFamily: "calibri",
+				fontColor: "dimGrey"
+			},
+			axisX: {
+				//title: "Time"
+			},
+			axisY:{
+				//title: "Acceleration",
+				includeZero: false,
+				gridDashType: "dot" 
+			}, 
+			data: [{ 
+				// dataSeries1
+				type: "line",
+				xValueType: "number",
+				showInLegend: true,
+				name: GYROline1 ,
+				dataPoints: GYROdataPoints1
+			},
+			{				
+				// dataSeries2
+				type: "line",
+				xValueType: "number",
+				showInLegend: true,
+				name: GYROline2 ,
+				dataPoints: GYROdataPoints2
+			},
+			{				
+				// dataSeries3
+				type: "line",
+				xValueType: "number",
+				showInLegend: true,
+				name: GYROline3 ,
+				dataPoints: GYROdataPoints3
+			}],
+          legend:{
+			dockInsidePlotArea: true,
+			horizontalAlign: "right",
+            cursor:"pointer",
+            itemclick : function(e) {
+              if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                e.dataSeries.visible = false;
+              }
+              else {
+                e.dataSeries.visible = true;
+              }
+              GYROchart.render();
+            }
+          }
+		});
+
+		var GYROupdateInterval = 1000;
+		// initial value
+		var GYROyValue1 = 0; 
+		var GYROyValue2 = 0; 
+		var GYROyValue3 = 0; 
+
+		var GYROtime = 0;
+
+		var GYROupdateChart = function (GYROcount) {
+			GYROcount = GYROcount || 1;
+
+			// count is number of times loop runs to generate random dataPoints. 
+
+			for (var i = 0; i < GYROcount; i++) {
+				
+				// add interval duration to time				
+				GYROtime++;
+
+
+				// generating random values
+				var GYROdeltaY1 = .5 + Math.random() *(-.5-.5);
+				var GYROdeltaY2 = .5 + Math.random() *(-.5-.5);
+				var GYROdeltaY3 = .5 + Math.random() *(-.5-.5);
+
+				// adding random value and rounding it to two digits. 
+				GYROyValue1 = Math.round((GYROyValue1 + GYROdeltaY1)*100)/100;
+				GYROyValue2 = Math.round((GYROyValue2 + GYROdeltaY2)*100)/100;
+				GYROyValue3 = Math.round((GYROyValue3 + GYROdeltaY3)*100)/100;
+				
+				// pushing the new values
+				GYROdataPoints1.push({
+					x: GYROtime,
+					y: GYROyValue1
+				});
+				GYROdataPoints2.push({
+					x: GYROtime,
+					y: GYROyValue2
+				});
+				GYROdataPoints3.push({
+					x: GYROtime,
+					y: GYROyValue3
+				});
+
+				// Delete old data to prevent build up
+				if (GYROdataPoints1.length > 30) {
+					GYROdataPoints1.shift();
+				}
+				if (GYROdataPoints2.length > 30) {
+					GYROdataPoints2.shift();
+				}
+				if (GYROdataPoints3.length > 30) {
+					GYROdataPoints3.shift();
+				}
+
+			};
+
+			GYROchart.render();
+
+		};
+
+		// generates first set of dataPoints 
+		GYROupdateChart(30);	
+		 
+		// update chart after specified interval 
+		setInterval(function(){GYROupdateChart()}, GYROupdateInterval);
+}
