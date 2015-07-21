f8e02dda9a424238ba5d7056ab5135e593a19703
Jonathan <jdeiven@outlook.com>
2015-07-20 20:01:07 -0400

    Navbar color changes to show status

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -4,6 +4,7 @@ window.onload = function () {
 	var lux = prompt("What is the lux reading?");
 	if (lux > 10) {
 		$("#offlineimage").css("visibility", "hidden");
+		$(".navbar").css("border-bottom-color", "#4d8cc5");
 	}
 
 	/*
